316d0ecfd572358bab234c705002aaf5028006ce
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-09-13 09:44:30 -0700

    Update pidei.sh

--- pidei.sh
+++ pidei.sh
@@ -34,12 +34,12 @@ SA MEANS STANDALONE EMU" 0 0
       14 "Commadore64<------------------------>" \
       15 "Colecovision<------------------------>" \
       16 "Dragon32<------------------------>" \
-      17 "Dreamcast                       12.6GB       19 GAMES" \
-      18 "Electron                         2.5MB      152 GAMES" \
-      19 "Famicon                           19MB      169 GAMES" \
-      20 "Famicom Disk                       2MB       43 GAMES" \
-      21 "Game and Watch                    48MB       53 GAMES" \
-      22 "Gameboy                           42MB      565 GAMES" \
+      17 "Dreamcast<------------------------>" \
+      18 "Electron<------------------------>" \
+      19 "Famicon<------------------------>" \
+      20 "Famicom Disk<------------------------>" \
+      21 "Game and Watch<------------------------>" \
+      22 "Gameboy<------------------------>MULTI CORES" \
       23 "Gameboy Advance                  3.4GB     1006 GAMES" \
       24 "Gameboy Color                    232MB      538 GAMES" \
       25 "Gamegear                          42MB      249 GAMES" \
@@ -78,87 +78,99 @@ SA MEANS STANDALONE EMU" 0 0
       58 "VideoPAC                         430KB       99 GAMES" \
       59 "Virtual Boy                        8MB       24 GAMES" \
       60 "Dreamcast VMU                      3MB      115 GAMES" \
-      61 "Wonderswan Color                 116MB       84 GAMES" \
-      62 "Sharp X1                         7.6MB       69 GAMES" \
-      63 "Sharp X68000                     504MB      418 GAMES" \
-      64 "ZMachine                           4MB       30 GAMES" \
-      65 "ZXSpectrum                        38MB     1111 GAMES" \
-	  66 "FORCE RETROPIE SETUP SCRIPT UPDATE                   " \
+      61 "Wine" \
+      62 "Wonderswan Color                 116MB       84 GAMES" \
+      63 "Sharp X1                         7.6MB       69 GAMES" \
+      64 "Sharp X68000                     504MB      418 GAMES" \
+      65 "ZMachine                           4MB       30 GAMES" \
+      66 "ZXSpectrum                        38MB     1111 GAMES" \
+       + "<----------------Pi.D.E.I Tools--------------------->" \
+      T1 "FORCE RETROPIE SETUP SCRIPT UPDATE                   " \
+      T2 "UPDATE PI.D.E.I                                      " \
       2>&1 >/dev/tty)
 
     case "$choice" in
-    1) install-emu "amiga" "lr-puae" ;;
-    2) install-emu "amigacd32" "lr-puae" ;;
-    3) install-emu "amstradcpc" "lr-caprice32" ;;
-    4) install-emu "arcade" "lr-mame2003" ;;
-    5) install-emu "arcadia" ;;
-    6) install-emu "astrocade" ;;
-    7) install-emu "atari800" ;;
-    8) install-emu "atari2600" "lr-stella" ;;
-    9) install-emu "atari5200" "lr-atari800";;
-    10) install-emu "atari7800" "lr-prosystem";;
-    11) install-emu "atarilynx" "lr-beetle-lynx" ;;
-    12) install-emu "atarist" "lr-hatari" ;;
-    13) install-emu "atomiswave" ;;
-    14) install-emu "c64" ;;
-    15) install-emu "coleco" "lr-bluemsx" ;;
-    16) install-emu "dragon32" "xroar" ;;
-    17) install-emu "dreamcast" "redream" ;;
-    18) install-emu "electron" ;;
-    19) install-emu "famicon" ;;
-    20) install-emu "fds" ;;
-    21) install-emu "gameandwatch" ;;
-    22) install-emu "gb" ;;
-    23) install-emu "gba" "lr-mgba" ;;
-    24) install-emu "gbc" ;;
-    25) install-emu "gamegear" "lr-genesis-plus-gx" ;;
-    26) install-emu "intellivision" "lr-freeintv" ;;
-    27) install-emu "lightgun" ;;
-    28) install-emu "markiii" ;;
-    29) install-emu "mastersystem" ;;
-    30) install-emu "megadrive" "lr-genesis-plus-gx" ;;
-    31) megadrive-japan ;;
-    32) install-emu "msx" ;;
-    33) install-emu "msx2" ;;
-    34) install-emu "n64" ;;
-    35) install-emu "naomi" ;;
-    36) install-emu "nds" ;;
-    37) install-emu "neogeo" ;;
-    38) install-emu "ngpc" ;;
-    39) install-emu "nes" ;;
-    40) install-emu "openbor" ;;
-    41) install-emu "oric" ;;
-    42) install-emu "pokemini" ;;
-    43) install-emu "psx" "lr-pcsx_rearmed" ;;
-    44) psp-pack ;;
-    45) model-3 ;;
-    46) install-emu "saturn" "lr-yabause" ;;
-    47) saturn-japan "saturn-japan" ;;
-    48) install-emu "scummvm" ;;
-    49) install-emu "sega32x" ;;
-    50) install-emu "segacd" "lr-picodrive" ;;
-    51) install-emu "sfc" ;;
-    52) install-emu "sg-1000" "lr-genesis-plus-gx" ;;
-    53) install-emu "snes" "lr-snes9x" ;;
-    54) msu1-pack ;;
-    55) install-emu "supergrafx" "lr-beetle" ;;
-    56) tg16-fix ;;
-    57) install-emu "vectrex" "lr-vecx" ;;
-    58) install-emu "videopac" ;;
-    59) install-emu "virtualboy" ;;
-    60) install-emu "svmu" ;;
-    61) install-emu "wonderswancolor" ;;
-    62) install-emu "x1" ;;
-    63) install-emu "x68000" ;;
-    64) install-emu "zmachine" ;;
-    65) install-emu "zxspectrum" ;;
-	66) 
+    1) install-emu "Amiga" "lr-puae" ;;
+    2) install-emu "Amigacd32" "lr-puae" ;;
+    3) install-emu "Amstradcpc" "lr-caprice32" ;;
+    4) multi-cores "Arcade" "lr-mame2003" ;;
+    5) install-emu "Arcadia" ;;
+    6) install-emu "Astrocade" ;;
+    7) install-emu "Atari800" ;;
+    8) install-emu "Atari2600" "lr-stella" ;;
+    9) install-emu "Atari5200" "lr-atari800";;
+    10) install-emu "Atari7800" "lr-prosystem";;
+    11) install-emu "Atarilynx" "lr-beetle-lynx" ;;
+    12) install-emu "Atarist" "lr-hatari" ;;
+    13) install-emu "Atomiswave" ;;
+    14) install-emu "C64" ;;
+    15) install-emu "Coleco" "lr-bluemsx" ;;
+    16) install-emu "Dragon32" "xroar" ;;
+    17) install-emu "Dreamcast" "redream" ;;
+    18) install-emu "Electron" ;;
+    19) install-emu "Famicon" ;;
+    20) install-emu "FDS" ;;
+    21) install-emu "Game&Watch" ;;
+    22) multi-cores "GameBoy" "lr-mgba" "lr-gb" ;;
+    23) install-emu "GameBoy Advacnce" "lr-mgba" ;;
+    24) install-emu "GameBoy Color" ;;
+    25) install-emu "Game Gear" "lr-genesis-plus-gx" ;;
+    26) install-emu "Intellivision" "lr-freeintv" ;;
+    27) install-emu "Lightgun" ;;
+    28) install-emu "Markiii" ;;
+    29) install-emu "Mastersystem" ;;
+    30) install-emu "Megadrive" "lr-genesis-plus-gx" ;;
+    31) install-emu "SNES" "lr-snes9x" ;;
+    32) install-emu "MSX" ;;
+    33) install-emu "MSX2" ;;
+    34) install-emu "N64" ;;
+    35) install-emu "Naomi" ;;
+    36) install-emu "NDS" ;;
+    37) install-emu "NeoGeo" ;;
+    38) install-emu "NeoGeo Pocket Color" ;;
+    39) install-emu "NES" ;;
+    40) install-emu "Openbor" ;;
+    41) install-emu "Oric" ;;
+    42) install-emu "Pokemini" ;;
+    43) install-emu "PS1" "lr-pcsx_rearmed" ;;
+    44) install-emu "PSP" "" ;;
+    45) install-emu "Sega Model-3" "lr-snes9x" ;;
+    46) install-emu "Sega Saturn" "lr-yabause" ;;
+    47) install-emu "Sega Saturn Japan" "lr-snes9x" ;;
+    48) install-emu "ScummVM" ;;
+    49) install-emu "Sega32x" ;;
+    50) install-emu "SegaCD" "lr-picodrive" ;;
+    51) install-emu "SFC" ;;
+    52) install-emu "SG-1000" "lr-genesis-plus-gx" ;;
+    53) install-emu "SNES" "lr-snes9x" ;;
+    54) install-emu "SNES-MSU1" "lr-snes9x" ;;
+    55) install-emu "SuperGrafx" "lr-beetle" ;;
+    56) install-emu "TurboGrafx-16" "lr-snes9x" ;;
+    57) install-emu "Vectrex" "lr-vecx" ;;
+    58) install-emu "Videopac" ;;
+    59) install-emu "Virtualboy" ;;
+    60) install-emu "SVmu" ;;
+    61) install-wine ;;
+    62) install-emu "Wonderswancolor" ;;
+    63) install-emu "X1" ;;
+    64) install-emu "X68000" ;;
+    65) install-emu "Zmachine" ;;
+    66) install-emu "ZXspectrum" ;; 
      +) none  ;;
+    T1) update-setup-script ;;
+    T2) update-pidei ;;
      *) break ;;
     esac
    done
 fi
 }
+
+##-----------------------------Example-----------------------------##
+##System-Name<------------------------>Core-Name
+##System-Name<------------------------>Mulit-Emus/Cores
+##install-emu "system-name" "lr-core1" "lr-core2" "lr-core3" "standalone emu1" "standalone emu2"
+##-----------------------------------------------------------------##
+
 function install-emu() {
 cd $HOME/RetroPie-Setup
 sudo ./retropie_packages.sh "$2"
@@ -167,10 +179,11 @@ sudo ./retropie_packages.sh "$2"
 function multi-cores() {
           whiptail --clear --title ""$1" Multi Core Menu" --separate-output --checklist "Choose Core(s) and click Download:" 0 0 0 \
       --ok-button Download --cancel-button Back \
-                "1" ""$2" Core" off \
-                "2" ""$3" Core" off \
-                "3" ""$4" Core" off \
-                "4" ""$5" Core" off \
+                "+" "<--->RetroArch Cores<--->" off \
+                "1" ""$2" Retroarch Core" off \
+                "2" ""$3" Retroarch Core" off \
+		"+" "<--->Standalone Emus<--->" off \
+                "3" ""$4" Standalone Emu" off \
                 2>/tmp/results
     while read -r choice  
         do
@@ -178,8 +191,8 @@ function multi-cores() {
 
             1) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$2" ;;
             2) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$3" ;;
-            3) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$4" ;;
-            4) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$5" ;;
+            4) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$4" ;;
+	    +) none ;;
             *) ;;
         esac
         done < /tmp/results
