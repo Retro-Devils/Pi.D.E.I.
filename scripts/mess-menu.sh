2b454ac4f714b470b23f61076637edf7d0c0ab11
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-10-12 12:00:08 -0600

    Update mess-menu.sh

--- scripts/mess-menu.sh
+++ scripts/mess-menu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function mess-menu() {
-if [ ! -f $HOME/pidei/mess-confirm.sh ]; then
+if [ ! -f $HOME/pidei/scripts/mess-confirm.sh ]; then
 dialog  --sleep 1 --title "MESS/MAME INSTALLED" --msgbox "
 MESS & MAME ARE ALREADY INSTALLED !!" 0 0
 bash $HOME/pidei/pidei.sh 
@@ -10,11 +10,11 @@ else
         choice=$(dialog --backtitle "DEVILS MESS INSTALLER" --title "1.1" \
             --ok-label OK --cancel-label Back \
             --menu "ARE MAME, LR-MESS & LR-MAME INSTALLED" 25 75 20 \
-	          1 "<-->YES THEY ARE INSTALLED<-->" \
+	    1 "<-->YES THEY ARE INSTALLED<-->" \
             2 "<-->NO THEY ARE NOT/UNSURE<-->" \
             2>&1 > /dev/tty)
         case "$choice" in
-	          1) installed ;;
+	    1) installed ;;
             2) not-installed ;;
             *)  break ;;
         esac
@@ -24,6 +24,9 @@ fi
 
 function installed() {
 bash $HOME/pidei/pidei.sh
+dialog  --sleep 1 --title "WARNING" --msgbox "
+PI DEI WILL NOW THINK MESS IS INSTALLED " 0 0
+wget https://raw.githubusercontent.com/Retro-Devils/Pi.D.E.I./main/scripts/mess-confirm.sh -P $HOME/pidei/scripts/
 }
 
 function not-installed() {
@@ -36,7 +39,7 @@ sudo ./retropie_packages.sh mame
 sleep 2
 cd
 sleep 1
-wget https://raw.githubusercontent.com/Retro-Devils/Pi.D.E.I./main/scripts/mess-confirm.sh -P $HOME/pidei/
+wget https://raw.githubusercontent.com/Retro-Devils/Pi.D.E.I./main/scripts/mess-confirm.sh -P $HOME/pidei/scripts/
 sudo cp /opt/retropie/libretrocores/lr-mess/mamemess_libretro.so -P /opt/retropie/libretrocores/lr-mess/mess_libretro.so
 wget https://raw.githubusercontent.com/Retro-Devils/Pi.D.E.I./main/scripts/run-mes.sh -P $HOME/RetroPie-Setup/scriptmodules/
 }
