8f973deb6bf6c241fa8dcf346a49c7a1c76e9111
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-10-14 18:56:41 -0600

    Update pidei-installer.sh

--- pidei-installer.sh
+++ pidei-installer.sh
@@ -33,10 +33,8 @@ function install-pidei() {
 if [ -d "$HOME/pidei/" ]; then sudo rm -R "$HOME"/pidei/; fi
 if [ -f "$HOME/RetroPie/retropiemenu/pidei.sh" ]; then sudo rm "$HOME"/RetroPie/retropiemenu/pidei.sh; fi
 if [ -f "/usr/local/bin/pidei" ]; then sudo rm /usr/local/bin/pidei; fi
-wget https://github.com/Retro-Devils/Pi.D.E.I./raw/main/intro.mp4 -P "$HOME"/pidei/
-wget https://raw.githubusercontent.com/Retro-Devils/Pi.D.E.I./main/pidei.sh -P "$HOME"/pidei/
-wget https://github.com/Retro-Devils/Pi.D.E.I./raw/main/pi-dei-logo.png -P "$HOME"/pidei/
-wget https://raw.githubusercontent.com/Retro-Devils/Pi.D.E.I./main/scripts/mess/mess-menu.sh -P $HOME/pidei/scripts/
+cd $HOME
+git clone https://github.com/Retro-Devils/Pi.D.E.I
 sudo wget -O "/usr/local/bin/pidei" https://raw.githubusercontent.com/Retro-Devils/Pi.D.E.I./main/pidei.sh
 cp "$HOME"/pidei/pidei.sh -f "$HOME"/RetroPie/retropiemenu/
 cp "$HOME"/pidei/pi-dei-logo.png -f "$HOME"/RetroPie/retropiemenu/icons/
