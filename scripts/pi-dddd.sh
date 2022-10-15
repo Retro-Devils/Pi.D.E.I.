3c2b44cd2becb1823962978ca703c21bb4d59f18
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-10-14 18:52:04 -0600

    Update pi-dddd.sh

--- scripts/pi-dddd.sh
+++ scripts/pi-dddd.sh
@@ -6,9 +6,9 @@ rpi=$(cat /proc/device-tree/model |awk '{print $3}')
 
 if [ "$rpi" = "Zero" ]; then bash $HOME/pidei/scripts/pidei-0.sh
 elif [ "$rpi" = "1" ]; then bash $HOME/pidei/scripts/pidei-0.sh
-elif [ "$rpi" = "2" ]; then bash $HOME/Devils-Box/scripts/pidei-2.sh
-elif [ "$rpi" = "3" ]; then bash $HOME/Devils-Box/scripts/pidei-2.sh
-elif [ "$rpi" = "4" ]; then bash $HOME/Devils-Box/scripts/pi-dei-4.sh
+elif [ "$rpi" = "2" ]; then bash $HOME/pidei/scripts/pidei-2.sh
+elif [ "$rpi" = "3" ]; then bash $HOME/pidei/scripts/pidei-2.sh
+elif [ "$rpi" = "4" ]; then bash $HOME/pidei/scripts/pi-dei-4.sh
 else
 {$HOME}/Devils-Box/scripts/pidei-other.sh
 fi
