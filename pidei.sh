73021c3d7cbec3016acd2b4da4c9fad778c9387f
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-10-20 06:28:01 -0600

    Update pidei.sh

--- pidei.sh
+++ pidei.sh
@@ -8,7 +8,7 @@ if [ "$rpi" = "Zero" ]; then bash $HOME/Pi.D.E.I./scripts/pidei-0.sh
 elif [ "$rpi" = "1" ]; then bash $HOME/Pi.D.E.I./scripts/pidei-0.sh
 elif [ "$rpi" = "2" ]; then bash $HOME/Pi.D.E.I./scripts/pidei-2.sh
 elif [ "$rpi" = "3" ]; then bash $HOME/Pi.D.E.I./scripts/pidei-2.sh
-elif [ "$rpi" = "4" ]; then bash $HOME/Pi.D.E.I./scripts/pi-dei-4.sh
+elif [ "$rpi" = "4" ]; then bash $HOME/Pi.D.E.I./scripts/pidei-4.sh
 else
 {$HOME}/Pi.D.E.I./scripts/pidei-other.sh
 fi
