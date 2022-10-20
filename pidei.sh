2b9741526d55f179877221c3023780a28afd7b44
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-10-19 20:57:21 -0500

    Update pidei.sh

--- pidei.sh
+++ pidei.sh
@@ -4,11 +4,11 @@ export NCURSES_NO_UTF8_ACS=1
 
 rpi=$(cat /proc/device-tree/model |awk '{print $3}')
 
-if [ "$rpi" = "Zero" ]; then bash $HOME/pidei/scripts/pidei-0.sh
-elif [ "$rpi" = "1" ]; then bash $HOME/pidei/scripts/pidei-0.sh
-elif [ "$rpi" = "2" ]; then bash $HOME/pidei/scripts/pidei-2.sh
-elif [ "$rpi" = "3" ]; then bash $HOME/pidei/scripts/pidei-2.sh
-elif [ "$rpi" = "4" ]; then bash $HOME/pidei/scripts/pi-dei-4.sh
+if [ "$rpi" = "Zero" ]; then bash $HOME/Pi.D.E.I./scripts/pidei-0.sh
+elif [ "$rpi" = "1" ]; then bash $HOME/Pi.D.E.I./scripts/pidei-0.sh
+elif [ "$rpi" = "2" ]; then bash $HOME/Pi.D.E.I./scripts/pidei-2.sh
+elif [ "$rpi" = "3" ]; then bash $HOME/Pi.D.E.I./scripts/pidei-2.sh
+elif [ "$rpi" = "4" ]; then bash $HOME/Pi.D.E.I./scripts/pi-dei-4.sh
 else
 {$HOME}/Devils-Box/scripts/pidei-other.sh
 fi
