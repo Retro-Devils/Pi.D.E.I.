140936cb94d548026a0ff9397bac550d54058ee0
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2023-01-06 07:19:01 -0700

    Update pidei-4.sh

--- scripts/pidei-4.sh
+++ scripts/pidei-4.sh
@@ -20,7 +20,7 @@ function emu-menu() {
       3 "AmigaCD32<-------------------->RA PUAE" \
       4 "Amstrad CPC<------------------>RA CAPRICE32" \
       5 "Arcade<----------------------->MULTI CORES/EMUS" \
-      6 "Arcadia<---------------------->" \
+      6 "Arcadia<---------------------->RA MESS" \
       7 "Astrocade<-------------------->RA MESS" \
       8 "Atari800<--------------------->RA ATARI800"\
       9 "Atari2600<-------------------->RA STELLA" \
@@ -28,7 +28,7 @@ function emu-menu() {
       11 "Atari7800<------------------->RA PROSYSTEM" \
       12 "AtariLynx<------------------->RA BEETLE LYNX" \
       13 "AtariST<--------------------->RA HATARI" \
-      14 "Atomiswave<------------------>" \
+      14 "Atomiswave<------------------>UNFINISHED" \
       15 "BBC Micro<------------------->EMU--B-EM" \
       16 "Commadore64<----------------->EMU--VICE" \
       17 "Colecovision<---------------->RA BLUEMSX" \
@@ -36,55 +36,55 @@ function emu-menu() {
       19 "Daphne<---------------------->EMU--DAPHNE" \
       20 "Dreamcast<------------------->MULTI CORES/EMUS" \
       21 "Electron<-------------------->RA MESS " \
-      22 "Famicon<--------------------->" \
-      23 "Famicom Disk<---------------->" \
-      24 "Game and Watch<-------------->" \
+      22 "Famicon<--------------------->RA NESTOPIA" \
+      23 "Famicom Disk<---------------->RA NESTOPIA" \
+      24 "Game and Watch<-------------->RA GW" \
       25 "Gameboy<--------------------->MULTI CORES/EMUS" \
       26 "Gameboy Advance<------------->MULTI CORES/EMUS" \
       27 "Gameboy Color<--------------->MULTI CORES/EMUS" \
       28 "Gamegear<-------------------->MULTI CORES/EMUS" \
       29 "Intellivision<--------------->RA FREEINTV" \
-      30 "MarkIII<--------------------->" \
-      31 "Mastersystem<---------------->" \
-      32 "Genesis/Megadrive<----------->" \
-      33 "Genesis/Megadrive-Japan<----->" \
+      30 "MarkIII<--------------------->UNFINSIHED" \
+      31 "Mastersystem<---------------->RA SMSPLUS GX" \
+      32 "Genesis/Megadrive<----------->RA GENESIS PLUS GX" \
+      33 "Genesis/Megadrive-Japan<----->RA GENESIS PLUS GX" \
       34 "MSX<------------------------->MULTI CORES/EMUS" \
       35 "MSX 2<----------------------->MULTI CORES/EMUS" \
       36 "Nintendo 64<----------------->MULTI CORES/EMUS" \
-      37 "Naomi<----------------------->" \
+      37 "Naomi<----------------------->UNFINISHED" \
       38 "Nintendo DS<----------------->MULTI CORES/EMUS" \
       39 "NeoGeo<---------------------->RA NEOCD" \
       40 "Neo Geo Pocket Color<-------->RA-BETTLE NGP" \
-      41 "NES<------------------------->" \
+      41 "NES<------------------------->MULTI CORES/EMUS" \
       42 "Openbor<--------------------->EMU--OPENBOR" \
-      43 "Oric<------------------------>" \
-      44 "Pokemini<-------------------->" \
+      43 "Oric<------------------------>UNFINISHED" \
+      44 "Pokemini<-------------------->RA POKEMINI" \
       45 "PlayStation 1<--------------->MULTI CORES/EMUS" \
       46 "Plug N Play<----------------->RA MESS SYSTEM" \
       47 "Pokitto<--------------------->EMU--POKITTO" \
       48 "PSP<------------------------->MULTI CORES/EMUS" \
-      49 "Sega Model 3<---------------->EMU--" \
-      50 "Sega Saturn<----------------->CORE/EMU" \
-      51 "Sega Saturn Japan<----------->CORE/EMU" \
-      52 "ScummVM<--------------------->CORE/EMU" \
-      53 "Sega32x<--------------------->CORE/EMU" \
-      54 "SegaCD<---------------------->CORE/EMU" \
-      55 "Super Famicon<--------------->CORE/EMU" \
-      56 "SG-1000<--------------------->CORE/EMU" \
-      57 "SNES<------------------------>CORE/EMU" \
-      58 "SNES MSU1<------------------->CORES/EMU" \
+      49 "Sega Model 3<---------------->EMU--SUPERMODEL3" \
+      50 "Sega Saturn<----------------->EMU-YABASANSHIRO" \
+      51 "Sega Saturn Japan<----------->EMU--YABASHANSIRO" \
+      52 "ScummVM<--------------------->RA SCUMVM" \
+      53 "Sega32x<--------------------->UNFINISHED" \
+      54 "SegaCD<---------------------->RA PICO DRIVE" \
+      55 "Super Famicon<--------------->RA SNES9X" \
+      56 "SG-1000<--------------------->RA GENESISS PLUS GX" \
+      57 "SNES<------------------------>RA SNES9X" \
+      58 "SNES MSU1<------------------->RA SNES9X" \
       59 "SuperGraffiix<--------------->RA BEETLE SUPERGRAX" \
       60 "Turbo Graffix 16/PC Engine<-->RA BEETLE PCE FAST" \
-      61 "Vectrex<--------------------->CORE/EMU" \
-      62 "VideoPAC<-------------------->CORE/EMU" \
-      63 "Virtual Boy<----------------->CORE/EMU" \
+      61 "Vectrex<--------------------->RA VEXC" \
+      62 "VideoPAC<-------------------->UNFINIHSED" \
+      63 "Virtual Boy<----------------->RA BETTLE VB" \
       64 "VMU Dreamcast<--------------->CORE/EMU" \
       65 "Wine<------------------------>EMU--WINE/BOX86" \
       66 "Wonderswan Color<------------>RA BEETLE WSWAN" \
-      67 "Sharp X1<-------------------->CORE/EMU" \
-      68 "Sharp X68000<---------------->CORE/EMU" \
-      69 "ZMachine<-------------------->CORE/EMU" \
-      70 "ZXSpectrum<------------------>CORE/EMU" \
+      67 "Sharp X1<-------------------->UNFINISHED" \
+      68 "Sharp X68000<---------------->RA PX68K" \
+      69 "ZMachine<-------------------->UNFINISHED" \
+      70 "ZXSpectrum<------------------>RA FUSE" \
        + "<                                     >" \
        + "<-----------Pi.D.E.I Tools------------>" \
        + "<                                     >" \
@@ -120,10 +120,10 @@ function emu-menu() {
     17) install-emu "Coleco" "lr-bluemsx" ;;
     18) install-emu "Dragon32" "xroar" ;;
     19) install-emu "Daphne" "daphne" ;;
-    20) install-emu "Dreamcast" "lr-dreamcast" "lr-flycast" "redream" ;;
+    20) multi-cores5 "Dreamcast" "lr-flycast" "redream" ;;
     21) mess-system "Electron" "electron";;
     22) install-emu "Famicon" "lr-nestopia";;
-    23) install-emu "FDS" ;;
+    23) install-emu "FDS" "lr-nestopia" ;;
     24) install-emu "Game&Watch" "lr-gw" ;;
     25) multi-cores "GameBoy" "lr-mgba" "lr-gb" ;;
     26) multi-cores "GameBoy Advacnce" "lr-mgba" "lr-gpsp" "NO";;
@@ -133,7 +133,7 @@ function emu-menu() {
     30) install-emu "Markiii" ;;
     31) install-emu "Mastersystem" "lr-smsplus-gx" ;;
     32) install-emu "Megadrive" "lr-genesis-plus-gx" ;;
-    33) install-emu "Megadrive Japan" "lr-snes9x" ;;
+    33) install-emu "Megadrive Japan" "lr-genesis-plus-gx" ;;
     34) multi-cores "MSX" "lr-bluemsx" "lr-fmsx" "----" ;;
     35) multi-cores "MSX2" "lr-bluemsx" "lr-fmsx" "----" ;;
     36) multi-cores "N64" "lr-parallel-n64" "lr-mupen64plus-next" "mupen64plus" ;;
@@ -148,14 +148,14 @@ function emu-menu() {
     45) multi-cores "PS1" "lr-pcsx_rearmed" ;;
     46) mess-system "plugnplay" ;;
     47) install-pokitto ;;
-    48) multi-cores "PSP" "lr-ppsspp" "----" "ppsspp" ;;
+    48) install-emu2 "PSP" "ppsspp" ;;
     49) install-model-3 ;;
-    50) install-emu "Sega Saturn" "lr-beetle-saturn" "lr-yabuse" "yabasanshiro" ;;
-    51) multi-cores "Sega Saturn Japan" "lr-beetle-saturn" "lr-yabuse" "yabasanshiro" ;;
+    50) install-emu2 "Sega Saturn" "yabasanshiro" ;;
+    51) install-emu2 "Sega Saturn Japan" "yabasanshiro" ;;
     52) install-emu "ScummVM" "lr-scummvm" "----" "scummvm" ;;
     53) install-emu "Sega32x" ;;
     54) install-emu "SegaCD" "lr-picodrive" ;;
-    55) install-emu "SFC" ;;
+    55) install-emu "SFC" "lr-snes9x" ;;
     56) install-emu "SG-1000" "lr-genesis-plus-gx" ;;
     57) multi-cores2 "SNES" "lr-snes9x" "lr-bsnes" "lr-snes9x2002" ;;
     58) install-emu "SNES-MSU1" "lr-snes9x" ;;
@@ -170,7 +170,7 @@ function emu-menu() {
     67) install-emu "X1" ;;
     68) install-emu "X68000" "lr-px68k" ;;
     69) install-emu "Zmachine" ;;
-    70) multi-cores "ZXspectrum" "lr-fuse" "----" "zesarux" ;;
+    70) install-emu "ZXspectrum" "lr-fuse" "----" "zesarux" ;;
      +) none  ;;
     T1) update-setup-script ;;
     T2) update-pidei ;;
@@ -187,6 +187,11 @@ function emu-menu() {
 function install-emu() {
 cd $HOME/RetroPie-Setup
 sudo ./retropie_packages.sh "$2" install_bin
+}
+
+function install-emu2() {
+cd $HOME/RetroPie-Setup
+sudo ./retropie_packages.sh "$2"
 }
 
 function multi-cores() {
@@ -226,7 +231,7 @@ function multi-cores2() {
             1) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$2" install_bin ;;
             2) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$3" install_bin ;;
             3) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$4" install_bin ;;
-            4) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$5" install_bin ;;
+            4) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$5" ;;
 	    +) none ;;
             *) ;;
         esac
@@ -253,12 +258,72 @@ function multi-cores3() {
             3) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$4" install_bin ;;
             4) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$5" install_bin ;;
             5) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$6" install_bin ;;
-            6) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$7" install_bin ;;
+            6) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$7" ;;
 	    +) none ;;
             *) ;;
         esac
         done < /tmp/results
 }
+
+function multi-cores() {
+          whiptail --clear --title "$1 Multi Core Menu" --separate-output --checklist "Choose Core(s) and click Download:" 0 0 0 \
+      --ok-button Install --cancel-button Back \
+                "+" "<--->RetroArch Cores<--->" off \
+                "1" ""$2" Retroarch Core" off \
+                "2" ""$3" Retroarch Core" off \
+		"+" "<--->Standalone Emus<--->" off \
+                "3" ""$4" Standalone Emu" off \
+                2>/tmp/results
+    while read -r choice
+        do
+        case $choice in
+            1) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$2" install_bin ;;
+            2) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$3" install_bin ;;
+            3) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$4" ;;
+	    +) none ;;
+            *) ;;
+        esac
+        done < /tmp/results
+}
+
+function multi-cores4() {
+          whiptail --clear --title "$1 Multi Core Menu" --separate-output --checklist "Choose Core(s) and click Download:" 0 0 0 \
+      --ok-button Install --cancel-button Back \
+                "+" "<--->RetroArch Cores<--->" off \
+                "1" ""$2" Retroarch Core" off \
+	        "+" "<--->Standalone Emus<--->" off \
+                "2" ""$5" Standalone Emu" off \
+                2>/tmp/results
+    while read -r choice
+        do
+        case $choice in
+            1) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$2" ;;
+            2) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$3" ;;
+	    +) none ;;
+            *) ;;
+        esac
+        done < /tmp/results
+}
+
+function multi-cores5() {
+          whiptail --clear --title "$1 Multi Core Menu" --separate-output --checklist "Choose Core(s) and click Download:" 0 0 0 \
+      --ok-button Install --cancel-button Back \
+                "+" "<--->RetroArch Cores<--->" off \
+                "1" ""$2" Retroarch Core" off \
+	        "+" "<--->Standalone Emus<--->" off \
+                "2" ""$5" Standalone Emu" off \
+                2>/tmp/results
+    while read -r choice
+        do
+        case $choice in
+            1) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$2" install_bin ;;
+            2) cd $HOME/RetroPie-Setup && sudo ./retropie_packages.sh "$3" ;;
+	    +) none ;;
+            *) ;;
+        esac
+        done < /tmp/results
+}
+
 
 
 ##-----------------------------UnOffical Emus/Cores-----------------------------##
